795ed032553c3c9634935153b5a4e09762cf213d
Marc Bolinas <bolinas.marc@gmail.com>
2018-12-04 18:09:02 -0500

    bug testing

--- test00.c
+++ test00.c
@@ -1,86 +1,55 @@
 /* 
- * Test Program #3 - Semaphore
+ * Test Program #6 - Mailbox
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "ud_thread.h"
 
-sem_t *s;
-int resource = 0;
+mbox *mb;
+char *msg[2] = {"hello world...", "bye, bye"};
 
-void read_function(int val) 
+void producer(int id) 
 {
-   long i, j ;
-   int res_before, res_after;
+  int i;
+  char mymsg[30];
 
-   for (i = 0; i < 5; i++) {
-      printf("I am READ thread %d (%d)\n", val, i);
+  for (i = 0; i < 2; i++) {
+    sprintf(mymsg, "%s - tid %d", msg[i], id);
+    printf("Producer (%d): [%s] [length=%d]\n", id, mymsg, strlen(mymsg));
+    mbox_deposit(mb, mymsg, strlen(mymsg));
+  }
 
-      sem_wait(s);
-            
-      res_before = resource;
-      printf("  [%d READ %d] resource = %d\n", val, i, resource);
-
-      t_yield();
-      printf("  ** [%d READ %d] returns from t_yield()\n", val, i);
-      res_after = resource;
-
-      if (res_before != res_after) {
-
-         fprintf(stderr, "[THREAD %d] error in the semaphore program \n", val);
-         exit(-1);
-      }
-
-      sem_signal(s);
-   }
-
-   t_terminate () ;
+  t_terminate();
 }
 
-void write_function(int val) 
+void consumer(int id) 
 {
-   long i, j ;
+  int i;
+  int len;
+  char mesg[1024];
 
-   for (i = 0; i < 5; i++) {
-      printf("I am WRITE thread %d (%d)\n", val, i);
+  for (i = 0; i < 4; i++) {
+    mbox_withdraw(mb, mesg, &len);
+    printf("Message from mailbox: [%s]\n", mesg);
+  }
 
-      sem_wait(s);
-            
-      resource = rand() % 100;      
-      printf("  [%d WRITE %d] resource = %d\n", val, i, resource);
-
-      sem_signal(s);
-
-      t_yield();
-   }
-
-   t_terminate();
+  t_terminate();
 }
 
 int main(void) {
 
-   int i;
-
    t_init();
-   sem_init(&s, 1);
 
-   t_create(write_function, 1, 1);
-   t_create(write_function, 2, 1);
-   t_create(read_function, 11, 1);
-   t_create(read_function, 22, 1);
-   t_create(write_function, 3, 1);
-   t_create(write_function, 4, 1);
-   t_create(read_function, 33, 1);
-   t_create(read_function, 44, 1);
-  
-   for (i = 0; i < 60; i++) {
-      printf("I am main thread (%d)...\n", i);
-      t_yield();
-   }
+   mbox_create(&mb);
+   t_create(producer, 1, 1);
+   t_create(producer, 2, 1);
+   t_create(consumer, 3, 1);  
+   t_yield();
+   mbox_destroy(&mb);
 
-   sem_destroy(&s);
    t_shutdown();
+   printf("Done with mailbox test...\n");
 
    return 0;
 }
