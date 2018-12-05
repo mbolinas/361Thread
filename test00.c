41b89bac203bee3612f9e12e9220a0eb82ca9760
Marc Bolinas <bolinas.marc@gmail.com>
2018-12-04 20:15:36 -0500

    bug testing

--- test00.c
+++ test00.c
@@ -1,58 +1,51 @@
-/*
- * Test Program #8 - Asynchronous Send/Receive
+/* 
+ * Test Program #09 - Rendezvous: blocking Send/Receive
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "ud_thread.h"
 
-void producer(int val) 
-{
-   char msg[] = "hello world...";
-   int i = 0;
+void producer(int val) {
 
-   for (i = 0; i < 4; i++) {
-      msg[12] = (char) i+0x30;
-      send(2, msg, strlen(msg));
-      printf("thread %d [%s]...\n", val, msg);
-   }
+   char *msg = "hello world...\0";
 
-   printf("thread %d terminates...\n", val);
-   t_terminate();
-}
-
-void consumer(int val) 
-{
-   printf("hello????\n");
-   int len, who = 0;
-   char *msg;
-
-   msg = (char *) malloc(1024);
-
-   do {
-      who = 0;
-      receive(&who, msg, &len);
-      if (who != 0)
-        printf("I got message [%s] from %d...\n", msg, who);
-   }
-   while (who != 0);
-   
-   free(msg);
+   printf("P: Before block_send()...\n");
+   block_send(2, msg, strlen(msg));
+   printf("P: After block_send()...\n");
 
    t_terminate();
 }
 
-int main(void) 
-{
-   int i;
+void consumer(int val) {
+
+   int len, who = 1;
+   char msg[1024];
+
+   printf("C: Before block_receive()...\n");
+   block_receive(&who, msg, &len);
+   printf("C: After block_receive()...\n");
+
+   if (len != 0) {
+     printf("C %d got message [%s] from P %d...\n", val, msg, who);
+   }
+
+   t_terminate();
+}
+
+int main(void) {
 
    t_init();
 
-   t_create(producer, 1, 1);
-   t_create(producer, 3, 1);
    t_create(consumer, 2, 1);
+   t_create(producer, 1, 1);
 
    t_yield();
+   printf("I am main 1...\n");
+   t_yield();
+   printf("I am main 2...\n");
+   t_yield();
+   printf("I am main 3...\n");
 
    t_shutdown();
 
